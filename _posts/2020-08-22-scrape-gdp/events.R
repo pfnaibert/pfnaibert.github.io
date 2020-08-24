68ee695cf623491e0fb193537be41418a2bcd025
pfnaibert <paulo.naibert@gmail.com>
2020-08-24 15:41:04 -0300

    deletion of previous post

--- _posts/2020-08-22-scrape-gdp/events.R
+++
@@ -1,7 +0,0 @@
-
-dyEvent("2002-01-1", "Lula",  labelLoc = "bottom") %>%
-dyEvent("2008-10-1", "Crise", labelLoc = "bottom") %>%
-dyEvent("2010-01-1", "Dilma", labelLoc = "bottom") %>%
-dyEvent("2016-09-1", "Temer", labelLoc = "bottom") %>%
-dyEvent("2019-01-1", "Bolsonaro", labelLoc = "bottom") %>%
-
--- _posts/2020-08-22-scrape-gdp/test.R
+++
@@ -1,8 +0,0 @@
-source("../../R/funs-macro.R")
-library(xts)
-# gdp.dl()
-
-gdp.transform()
-gdp.df  <- gdp.load.df() ; head(gdp.df)
-gdp.xts <- gdp.load.xts(); head(gdp.xts)
-
